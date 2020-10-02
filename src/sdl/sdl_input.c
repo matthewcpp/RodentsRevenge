00c40dc6c1be08b2f52c7ebb92f527ba718a87dc
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-10-02 06:16:10 -0700

    Add Pointer to input

--- src/sdl/sdl_input.c
+++ src/sdl/sdl_input.c
@@ -9,9 +9,16 @@ typedef struct {
     int hold_time;
 } rrSDLButtonInfo;
 
+typedef struct {
+    int prev_state;
+    int cur_state;
+    rrPoint position;
+} rrSDLPointerInfo;
+
 struct rrInput{
     SDL_GameController* controller;
     rrSDLButtonInfo _button_info[RR_INPUT_BUTTON_COUNT];
+    rrSDLPointerInfo _pointer;
 };
 
 rrInput* rr_sdl_input_create(int joystick_index, const char* mapping_file_path) {
@@ -26,6 +33,7 @@ rrInput* rr_sdl_input_create(int joystick_index, const char* mapping_file_path) 
 
     input->controller = controller;
     memset(input->_button_info, 0, sizeof(rrSDLButtonInfo) * RR_INPUT_BUTTON_COUNT);
+    memset(&input->_pointer, 0, sizeof(rrSDLPointerInfo));
 
     return input;
 }
@@ -47,6 +55,18 @@ int rr_input_button_down(rrInput* input, rrInputButton button) {
 
 int rr_input_button_held_time(rrInput* input, rrInputButton button) {
     return input->_button_info[button].hold_time;
+}
+
+int rr_input_pointer_down(rrInput* input){
+    return input->_pointer.cur_state == 0 && input->_pointer.prev_state == 1;
+}
+
+int rr_input_pointer_up(rrInput* input){
+    return input->_pointer.cur_state == 1 && input->_pointer.prev_state == 0;
+}
+
+void rr_input_pointer_pos(rrInput* input, rrPoint* pos) {
+    rr_point_copy(pos, &input->_pointer.position);
 }
 
 void rr_sdl_input_update_button(rrInput* input, rrInputButton button, int value, int time) {
@@ -84,7 +104,15 @@ void rr_sdl_update_keyboard(rrInput* input, int time) {
     rr_sdl_input_update_button(input, RR_INPUT_BUTTON_BACK, keyboard_state[SDL_SCANCODE_ESCAPE], time);
 }
 
+void rr_sdl_update_pointer(rrInput* input) {
+    Uint32 mouse_state = SDL_GetMouseState(&input->_pointer.position.x, &input->_pointer.position.y);
+    input->_pointer.prev_state = input->_pointer.cur_state;
+    input->_pointer.cur_state = mouse_state & SDL_BUTTON(SDL_BUTTON_LEFT);
+}
+
 void rr_sdl_input_update(rrInput* input, int time) {
+    rr_sdl_update_pointer(input);
+
     if (input->controller)
         rr_sdl_input_update_joystick(input, time);
     else
